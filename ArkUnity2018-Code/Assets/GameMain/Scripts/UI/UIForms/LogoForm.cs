49a929245a74d1ed929aec883df85883cee659ac
Meng Dong <15093138420@163.com>
2018-02-13 21:26:28 +0800

    multable platfom。

--- ArkUnity2018-Code/Assets/GameMain/Scripts/UI/UIForms/LogoForm.cs
+++ ArkUnity2018-Code/Assets/GameMain/Scripts/UI/UIForms/LogoForm.cs
@@ -6,14 +6,23 @@ namespace ARKGame
     public class LogoForm : UGuiForm
     {
 
+#if UNITY_2017_3_OR_NEWER
+        protected override void OnOpen(object userData)
+#else
         protected internal override void OnOpen(object userData)
+#endif
         {
             base.OnOpen(userData);
         }
 
+#if UNITY_2017_3_OR_NEWER
+        protected override void OnClose(object userData)
+#else
         protected internal override void OnClose(object userData)
+#endif
         {
             base.OnClose(userData);
+
         }
     }
 }
