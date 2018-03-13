9225443985335ad0009c480ee74ba270ddcf176b
Meng Dong <15093138420@163.com>
2018-03-13 14:04:18 +0800

    update and add build dep bat files.

--- ArkUnity2018-Code/build_dep_ArkGameClient_only.bat
+++ ArkUnity2018-Code/build_dep_ArkGameClient_only.bat
@@ -2,15 +2,6 @@
 
 echo Building dependencies...
 cd Assets
-
-REM ######################################################################################################
-echo Building GameFramework...
-
-if exist GameFramework (rd GameFramework /q /s)
-git clone https://github.com/EllanJiang/UnityGameFramework.git
-
-echo Building GameFramework Success.
-
 cd GameMain
 REM ######################################################################################################
 echo BUilding ArkGameClient
