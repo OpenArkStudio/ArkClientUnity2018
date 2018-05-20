d4f2be8dd04ab7c476ccb2d3d2c6c03f3a78a164
Meng Dong <15093138420@163.com>
2018-05-20 09:36:10 +0800

    modify build_dep.bat

--- ArkUnity2018-Code/build_dep.bat
+++ ArkUnity2018-Code/build_dep.bat
@@ -10,14 +10,4 @@ if exist GameFramework (rd GameFramework /q /s)
 git clone https://github.com/EllanJiang/UnityGameFramework.git
 
 echo Building GameFramework Success.
-
-cd GameMain
-REM ######################################################################################################
-echo BUilding ArkGameClient
-
-if exist ArkGameClient (rd ArkGameClient /q /s)
-git clone https://github.com/ArkGame/ArkGameClient.git
-
-cd ArkGameClient
-call build_dep.bat
 echo Building ArkGameClient Success.
